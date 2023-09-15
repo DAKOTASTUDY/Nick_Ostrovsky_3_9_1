748929ce7e2e595cfd65e2c79abbafedb09e17a5
Nick Ostrovsky <legatus357@gmail.com>
2023-09-15 14:06:59 -0700

    Add project files.

---
+++ Nick_Ostrovsky_3_9_1/Nick_Ostrovsky_3_9_1.cpp
@@ -0,0 +1,80 @@
+
+
+#include <iostream>
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+
+	bool operator > (Fraction f) 
+	{
+		return ((numerator_ * f.denominator_) > (f.numerator_ * denominator_));
+	
+	};
+	bool operator < (Fraction f)
+	{
+		return ((numerator_ * f.denominator_) < (f.numerator_ * denominator_));
+
+	};
+	/*bool operator > (Fraction f)
+	{
+		return ((numerator_ * f.denominator_) > (f.numerator_ * denominator_));
+
+
+	};*/
+	bool operator >= (Fraction f)
+	{
+		return ((numerator_ * f.denominator_) >= (f.numerator_ * denominator_));
+
+	};
+	bool operator <= (Fraction f)
+	{
+		return ((numerator_ * f.denominator_) <= (f.numerator_ * denominator_));
+
+	};
+	bool operator == (Fraction f)
+	{
+		return ((numerator_ * f.denominator_) == (f.numerator_ * denominator_));
+
+	};
+	bool operator != (Fraction f)
+	{
+		return ((numerator_ * f.denominator_) != (f.numerator_ * denominator_));
+
+	};
+};
+
+
+
+int main()
+{
+
+
+	Fraction f1(4, 3);
+	Fraction f2(6, 11);
+	std::cout <<std::boolalpha;
+	std::cout <<  "\t f1 > f2\t" << (f1 > f2) << std::endl;
+	std::cout << "\t f1 < f2\t" << (f1 < f2) << std::endl;
+	std::cout << "\t f1 >= f2\t" << (f1 >= f2) << std::endl;
+	std::cout << "\t f1 <= f2\t" << (f1 <= f2) << std::endl;
+	std::cout << "\t f1 == f2\t" << (f1 == f2) << std::endl;
+	std::cout << "\t f1 != f2\t" << (f1 != f2) << std::endl;
+	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+	return 0;
+}
+
+
